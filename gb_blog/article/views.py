1f6e8a29dd23bf78ae10ad46f7a86dc85c1662f5
Bingochen <chen@BingochendeMacBook-Pro.local>
2018-10-08 23:43:18 +0800

    done with tutorial

---
+++ gb_blog/article/views.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+
+from django.contrib.syndication.views import Feed
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger  #添加包
+
+import logging
+
+logger = logging.getLogger("django")
+
+
+# Create your views here.
+
+class RSSFeed(Feed) :
+    title = "RSS feed - article"
+    link = "feeds/posts/"
+    description = "RSS feed - blog posts"
+
+    def items(self):
+        return Article.objects.order_by('-date_time')
+
+    def item_title(self, item):
+        return item.title
+
+    def item_pubdate(self, item):
+        return item.add_date
+
+    def item_description(self, item):
+        return item.content
+    
+    def item_link(self, item):
+        return item.link
+
+
+def home(request):
+    return HttpResponse("Hello World, Django")
+
+# def detail(request, my_args):
+#     return HttpResponse("You're looking at my_args %s." % my_args)
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post' : post})
+
+def test(request) :
+    return render(request, 'home.html', {'current_time': datetime.now()})
+
+def home(request):
+    posts = Article.objects.all()  #获取全部的Article对象
+    paginator = Paginator(posts, 2) #每页显示两个
+    page = request.GET.get('page')
+    try :
+        post_list = paginator.page(page)
+    except PageNotAnInteger :
+        post_list = paginator.page(1)
+    except EmptyPage :
+        post_list = paginator.paginator(paginator.num_pages)
+    logger.warn(post_list.paginator.num_pages)
+    return render(request, 'home.html', {'post_list' : post_list})
+
+def archives(request) :
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list, 
+                                            'error' : False})
+
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+def search_tag(request, tag) :
+    try:
+        post_list = Article.objects.filter(category__iexact = tag) #contains
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'tag.html', {'post_list' : post_list})
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains = s)
+            if len(post_list) == 0 :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : True})
+            else :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : False})
+    return redirect('/')
+
+    
